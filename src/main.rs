4c337ee4b0e1d0e17c2cffee66752e9d84c9c721
livingcodes <case.joyce@gmail.com>
2023-03-26 21:23:58 -0500

    stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
 fn main() {
-    println!("hello world");
+    loop {
+        let mut guess = String::new();
+        println!("what's the num?");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        if guess.trim() == "exit" {
+            break
+        }
+        let num = guess.trim().parse::<i32>()
+            .expect("Failed to parse");
+        println!("num: {}", num);
+
+        if num == 12 {
+            println!("correct");
+            break
+        } else {
+            println!("try again");
+        }
+    }
 }
